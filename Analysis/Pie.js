71d2b53e41c71d0f9c61c7152753e9fe8dd53a67
velasciraptor <amyvelasquez432@gmail.com>
2022-01-19 17:50:12 -0600

    Coloring pie chart

--- Analysis/Pie.js
+++ Analysis/Pie.js
@@ -5,18 +5,19 @@ var pieData = d3.json('Data/cause_pie_chart.json').then(function(data) {
     console.log(info);
     console.log(pieNums);
 });
-var colors = ['Thistle', 'Tan', 'CornflowerBlue', 'DarkOliveGreen', 'LightPurple', 'GoldenRod', 'Sienna','Salmon'];
+// var colors = ['Thistle', 'Tan', 'CornflowerBlue', 'DarkOliveGreen', 'LightPurple', 'GoldenRod', 'Sienna','Salmon'];
 
 var trace = {
     labels: ['Corrosion Failure','Equipment Failure','Excavation Damage', 'Incorrect Operation',
      'Material Failure of Pipe or Weld','Natural Force Damage','Other Incident Cause', 'Other Outside Force Damage'],
     values: [944, 2124, 165, 674, 310, 217, 114, 91],
     marker: {
-        colors: colors},
+        colorscale: 'Electric'},
     type: 'pie'
 };
 
-var data = [trace]
+var data = [trace];
+
 var layout = {
     title: 'Cause by Incident (2010 - November 2021)',
 };
